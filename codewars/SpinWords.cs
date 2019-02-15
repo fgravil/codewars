810163b6af08e32ad4ab1ed1607fe0353e4b3854
Fred <fgravil18@gmail.com>
2019-02-14 19:55:26 -0500

    initial commit

---
+++ codewars/SpinWords.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace codewars
+{
+    public class SpinWords
+    {
+        public static string SpinWords(string sentence)
+        {
+            string newSentence = "";
+            var words = sentence.Split(" ");
+
+            foreach (string word in words)
+            {
+                var modifiedWord = (word.Length >= 5) ? reverseWord(word) : word;
+                newSentence = newSentence + " " + modifiedWord;
+            }
+
+            return newSentence.Substring(1);
+        }
+
+        public static string reverseWord(string word)
+        {
+            string newWord = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                newWord = newWord + word[i];
+            }
+
+            return newWord;
+        }
+    }
+}
---
+++ codewars/TenMinuteWalk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars
+{
+    public class TenMinuteWalk
+    {
+        public static bool IsValidWalk(string[] walk)
+        {
+            if (walk.Length != 10) return false;
+            var eastCnt = 0;
+            var westCnt = 0;
+            var northCnt = 0;
+            var southCnt = 0;
+
+            foreach (var item in walk)
+            {
+                if (item == "e") eastCnt++;
+                else if (item == "w") westCnt++;
+                else if (item == "n") northCnt++;
+                else if (item == "s") southCnt++;
+            }
+            return eastCnt == westCnt && northCnt == southCnt;
+        }
+    }
+}
