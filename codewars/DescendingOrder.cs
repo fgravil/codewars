80abb10aa1ef68b160268278cec98e79dd04501a
Fred <fgravil18@gmail.com>
2019-02-16 17:14:51 -0500

    added descending order

---
+++ codewars/DescendingOrder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace codewars
+{
+    public class DescendingOrder
+    {
+        public static int Descending(int num)
+        {
+            List<int> numList = new List<int>();
+            string numString = "";
+
+            while (num > 0)
+            {
+                numList.Add(num % 10);
+                num /= 10;
+            }
+            numList.Sort();
+            numList.ForEach(val => numString = val + numString);
+
+            return Convert.ToInt32(numString);
+        }
+
+        public static int Descending2(int num)
+        {
+            return int.Parse(string.Concat(num.ToString().OrderByDescending(x => x)));
+        }
+    }
+}
