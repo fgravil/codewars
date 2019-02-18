71b40ea318a8b3178cb116cfaca18b627d3b9cbc
Fred <fgravil18@gmail.com>
2019-02-18 00:23:54 -0500

    added new classes

---
+++ codewars/Squares.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars
+{
+    public class Squares
+    {
+        public static bool IsSquare(int n)
+        {
+            if (n < 0) return false;
+            if (n == 0) return true;
+
+            for(int i = 0; i <= n / 2; i++)
+            {
+                if (i * i == n) return true;
+
+                if (i * i > n) break;
+            }
+
+            return false;
+        }
+
+        public static bool IsSquare2(int n)
+        {
+            return Math.Sqrt(n) % 1 == 0;
+        }
+    }
+}
---
+++ codewars/Tickets.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codewars
+{
+    public class Tickets
+    {
+        public static string GetChange(int[] peopleInLine)
+        {
+            string notEnough = "NO";
+
+            if (peopleInLine[0] > 25 || peopleInLine[1] > 25)
+            {
+                return notEnough;
+            }
+
+            for(int i = 3; i < peopleInLine.Length; i++)
+            {
+                var price = peopleInLine[i];
+                if (price > 25)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        var exchange = price - peopleInLine[j];
+                        if(exchange >= 25)
+                        {
+                            price = exchange;
+                            peopleInLine[j] = 0;
+                        }
+                    }
+                }
+                if (price == 25) peopleInLine[i] = 25;
+                else return notEnough;
+            }
+
+            return "YES";
+        }
+    }
+}
